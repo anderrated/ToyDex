1b40a2a59e01992cf7daa8a552eaa18c90acc55d
Young <197771340+YoungFEK@users.noreply.github.com>
2025-05-29 10:13:03 +0800

    young1-branch

--- home.php
+++ home.php
@@ -8,6 +8,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>ToyDex</title>
+
+    <style>
+
+        /* for larger section */
+        .add_Popup_Form{
+        display: none;
+        position: fixed;
+        bottom: 25%;
+        right: 35%;
+        border: none;
+        z-index: 9;
+        width: 20%;
+        height: 400px;
+        padding: 10px;
+        background-color: white;
+        border-radius: 50px;
+        font-family: Arial;
+        }
+
+        /* for form */
+        /* .text_section{
+        width: 45%;
+        } */
+
+        .add_Popup_Input {
+        width: 95%;
+        height: 25px;
+        background-color:rgb(166, 0, 255);
+        border-radius: 50px;
+        border: none; 
+        color: white;
+        
+        }
+
+        .add_Popup_Label {
+        color:rgb(166, 0, 255);
+        font-weight: light;
+        font-size: 10px;
+        font-family: Arial;
+        }
+
+        .add_Popup_Form h2{
+        color:rgb(0, 0, 0);
+        font-weight: 100;
+        font-size: 20px;
+        font-family: Arial;
+        }
+
+        .addToyButton{
+        background-color: rgba(0, 247, 255, 0.91);
+        }
+
+        .close_addPop{
+        background-color: rgba(255, 0, 174, 0.91);
+        }
+
+        .addToyButton, .close_addPop{
+        color: white;
+        align-self: center;
+        border-radius: 50px;
+        width: 200px;
+        border: none;
+        margin-top: 10px;
+        }
+
+        .input_division{
+        width:250px;
+        }
+
+        
+
+    </style>
+
 </head>
 <body>
     <section class="home">
@@ -81,7 +154,10 @@
                         </div>
                     </li> -->
                     <?php
-                        $sql = "SELECT toyitem.name, toyitem.description, toyitem.item_id, toyimage.image_url, toyitem.brand, toyitem.category FROM toyitem, toyimage WHERE toyitem.item_id = toyimage.item_id";
+                        // $sql = "SELECT toyitem.name, toyitem.description, toyitem.item_id, toyimage.image_url, toyitem.brand, toyitem.category FROM toyitem INNER JOIN toyimage ON toy WHERE toyitem.item_id = toyimage.item_id";
+
+                        $sql = "SELECT toyitem.name, toyitem.description, toyitem.item_id, toyimage.image_url, toyitem.brand, toyitem.category FROM toyitem INNER JOIN has ON toyitem.item_id = has.item_id INNER JOIN toyimage ON toyimage.image_id = has.image_id";
+
                         $result = $conn->query($sql);
 
                         if ($result->num_rows > 0) {
@@ -113,9 +189,14 @@
                 </ul>
             </div>
         </section>
+
+
     <div class="add">
-        <img src="images/add-circle-svgrepo-com.svg" id="add-item">
+        <!-- <img src="images/add-circle-svgrepo-com.svg" id="add-item"> -->
+        <button id="add-item" onclick = "openAddPopup()">add item</button>
     </div>
+
+
     </section>
     <footer>
         <div class="logo">
@@ -144,5 +225,63 @@
             </div>
         </div>
     </footer>
+
+    <!-- <form action = "/action.php" class="add_Popup_Form" id = "add_Popup_Form" method=post>
+        <div class ="text_section">
+            <h2>Add a New Toy</h2>
+
+            <label for="Toy_Name" class = "add_Popup_Label">Toy Name</label>
+            <input type = "text" class = "add_Popup_Input" name ="Toy_Name" required>
+
+            <label for="Description" class = "add_Popup_Label">Description</label>
+            <input type = "text" class = "add_Popup_Input" name ="Description" required>
+
+            <label for="Price" class = "add_Popup_Label">Price</label>
+            <input type = "text" class = "add_Popup_Input" name ="Price" required>
+
+            <label for="Manufacturer" class = "add_Popup_Label">Manufacturer</label>
+            <input type = "text" class = "add_Popup_Input" name ="Manufacturer" required>
+
+            <label for="Category" class = "add_Popup_Label">Category</label>
+            <input type = "text" class = "add_Popup_Input" name ="Category" required>
+
+            <button type="submit" class="addToyButton">Add Toy</button>
+            <button type="button" class="close_addPop" onclick="closeAddPopup()">Cancel</button>
+        </div>
+    </form> -->
+
+    <form action = "add_toy.php" class="add_Popup_Form" id = "add_Popup_Form" method=post>
+        <h2>Add a New Toy</h2>
+
+        <label for="item_id" class = "add_Popup_Label">item id</label>
+        <input type = "text" class = "add_Popup_Input" name ="item_id" required>
+
+        <label for="Toy_Name" class = "add_Popup_Label">Toy Name</label>
+        <input type = "text" class = "add_Popup_Input" name ="Toy_Name" required>
+
+        <label for="Description" class = "add_Popup_Label">Description</label>
+        <input type = "text" class = "add_Popup_Input" name ="Description" required>
+
+        <label for="brand" class = "add_Popup_Label">Brand</label>
+        <input type = "text" class = "add_Popup_Input" name ="brand" required>
+
+        <label for="Category" class = "add_Popup_Label">Category</label>
+        <input type = "text" class = "add_Popup_Input" name ="Category" required>
+
+        <label for="Price" class = "add_Popup_Label">Price</label>
+        <input type = "text" class = "add_Popup_Input" name ="Price" required>
+
+        <!-- <label for="Manufacturer" class = "add_Popup_Label">Manufacturer</label>
+        <input type = "text" class = "add_Popup_Input" name ="Manufacturer" required> -->
+
+        <button type="submit" class="addToyButton">Add Toy</button>
+        <button type="button" class="close_addPop" onclick="closeAddPopup()">Cancel</button>
+    </form>
+   
+
+    <!-- <input type="text" class="imageUrl" placeholder="Enter image URL"> <img src="" alt="Image" class="preview"> <button>Delete</button> -->
+
+    <script src="main.js"> </script>
+
 </body>
 </html>
